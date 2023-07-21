6b2f9554b89d46ced683669d423cf74168042f93
Connor Denman <connor.denman@gmail.com>
2023-07-20 22:29:39 -0600

    collect the calories for each elf, sort, and sum the top 3

--- python/1/main.py
+++ python/1/main.py
@@ -2,19 +2,16 @@ def main():
     with open('input.txt') as f:
         lines = f.readlines()
 
-    most_calories = 0
+    elf_calories = []
     current_calories = 0
     for l in lines:
         if l == '\n':
-            if current_calories > most_calories:
-                most_calories = current_calories
-            
-            current_calories = 0
+           elf_calories.append(current_calories) 
+           current_calories = 0
         else:
             current_calories += int(l.replace('\n', ''))
     
-    print(most_calories)
-        
+    print(sum(sorted(elf_calories, reverse=True)[0:3])) 
 
 if __name__ == '__main__':
     main()

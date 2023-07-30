6df8abae5253f95be3e920b30987d3f9bd4b127b
Connor Denman <connor.denman@gmail.com>
2023-07-30 16:40:01 -0600

    solve day 2 part 2, plus allow for using test or real input based on argv input

--- python/2/main.py
+++ python/2/main.py
@@ -1,35 +1,39 @@
+import sys
+
 RPS_WINS = {
-    'X': 'C',
-    'Y': 'A',
-    'Z': 'B',
+    'A': 'B',
+    'B': 'C',
+    'C' : 'A',
 }
-
-RPS_TIES = {
-    'X': 'A',
-    'Y': 'B',
-    'Z': 'C',
+RPS_LOSSES = {
+    'A': 'C',
+    'B': 'A',
+    'C': 'B',
 }
-
 RPS_EXTRA_SCORE = {
-    'X': 1,
-    'Y': 2,
-    'Z': 3,
+    'A': 1,
+    'B': 2,
+    'C': 3,
 }
 
-def main():
-    with open('input.txt') as f:
+def main(argv):
+    with open(f'{argv[0]}.txt') as f:
         lines = f.readlines()
 
     my_score = 0
     for l in lines:
-        my_score += RPS_EXTRA_SCORE[l[2]]
-
-        if RPS_WINS[l[2]] == l[0]:
-            my_score += 6
-        elif RPS_TIES[l[2]] == l[0]:
+        if l[2] == 'X':
+            my_choice = RPS_LOSSES[l[0]]
+        elif l[2] == 'Y':
+            my_choice = l[0]
             my_score += 3
+        elif l[2] == 'Z':
+            my_choice = RPS_WINS[l[0]]
+            my_score += 6
+
+        my_score += RPS_EXTRA_SCORE[my_choice]
     
     print(my_score)
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:] if len(sys.argv) > 1 else ['input'])

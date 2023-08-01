2bbb5dc01308856d23c1dc46851ec4f61caa037f
Connor Denman <connor.denman@gmail.com>
2023-07-31 21:53:21 -0600

    calculate a priority sum, but not the right one

--- python/3/main.py
+++ python/3/main.py
@@ -1,27 +1,25 @@
 import sys
 
-PRIOTIES = {
+PRIORITIES = {
     **{letter: ord(letter) - 96 for letter in map(chr, range(97, 123))},
     **{letter: ord(letter) - 38 for letter in map(chr, range(65, 91))},
 }
 
-print(PRIOTIES)
-
 def main(argv):
-    # with open(f'{argv[0]}.txt') as f:
-        # lines = f.readlines()
+    with open(f'{argv[0]}.txt') as f:
+        lines = f.readlines()
 
     priorities_sum = 0
     for l in lines:
-
         items = dict()
         for i, c in enumerate(l):
             if i < len(l) / 2:
                items[c] = 1
             else:
-                pass
-                # if items.get(c, None):
+                if items.get(c, None):
+                    priorities_sum += PRIORITIES[c]
 
+    print(priorities_sum)
 
-# if __name__ == '__main__':
-    # main(sys.argv[1:] if len(sys.argv) > 1 else ['input'])
+if __name__ == '__main__':
+    main(sys.argv[1:] if len(sys.argv) > 1 else ['input'])

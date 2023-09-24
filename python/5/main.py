91e4d80cbb1185991ffd4a0896514c67d2e00982
Connor Denman <connor.denman@gmail.com>
2023-09-24 11:27:48 -0600

    parsing instructions and printing the modified crate stacks

--- python/5/main.py
+++ python/5/main.py
@@ -9,23 +9,26 @@ def main(argv):
     parsing_crates = True
 
     for row, l in enumerate(lines):
-        if l == '\n':
+        if l == '\n' or l.replace(' ', '').replace('\n', '').isdecimal():
+            print(crate_stacks)
             parsing_crates = False
-
-        for col, c in enumerate(l):
-            if c.isdecimal():
-                parsing_crates = False
-                continue
-
-            if parsing_crates:
+            continue
+        if parsing_crates:
+            for col, c in enumerate(l):
                 if (col - 1) % 4 == 0:
                     crate_index = int((col - 1) / 4)
                     try:
                         crate_stacks[crate_index].append(c)
                     except:
                         crate_stacks.append([c])
-            else:
-                pass
+        else:
+            parsed_instruction = l.split(' ')
+            move_count: int = int(parsed_instruction[1])
+            from_index: int = int(parsed_instruction[3]) - 1
+            to_index: int = int(parsed_instruction[5]) - 1
+
+            for _ in range(move_count):
+                crate_stacks[to_index].append(crate_stacks[from_index].pop())
 
     print(crate_stacks)
 

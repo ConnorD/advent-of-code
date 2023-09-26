e0a52b07bbe089bf4488289a2c391081a3bcf20f
Connor Denman <connor@amprobotics.com>
2023-09-25 23:00:35 -0600

    print the output in the correct format

--- python/5/main.py
+++ python/5/main.py
@@ -11,6 +11,7 @@ def main(argv):
     for row, l in enumerate(lines):
         if l == '\n' or l.replace(' ', '').replace('\n', '').isdecimal():
             parsing_crates = False
+            print('Parsed crate:', crate_stacks)
             continue
 
         if parsing_crates:
@@ -20,7 +21,7 @@ def main(argv):
 
                     try:
                         if c != ' ':
-                            crate_stacks[crate_index].append(c)
+                            crate_stacks[crate_index].insert(0, c)
                     except:
                         crate_stacks[crate_index] = [c]
         else:
@@ -28,11 +29,12 @@ def main(argv):
             move_count: int = int(parsed_instruction[1])
             from_index: int = int(parsed_instruction[3]) - 1
             to_index: int = int(parsed_instruction[5]) - 1
-            print(move_count, from_index, to_index)
-            for _ in range(move_count - 1):
-                crate_stacks[to_index].append(crate_stacks[from_index].pop())
+            print(crate_stacks)
 
-    print(crate_stacks)
+            for _ in range(move_count - 1):
+                crate_stacks[to_index].extend(crate_stacks[from_index].pop())
+
+    print(''.join(v[-1] for k, v in crate_stacks.items()))
 
 if __name__ == '__main__':
     main(sys.argv[1:] if len(sys.argv) > 1 else ['input'])
